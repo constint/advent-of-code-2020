b312b45625249f6f5096ab245d6ca5fce0b26e23
constint <cmanteau@talend.com>
2020-12-01 22:47:44 +0100

    Day1 solution

--- src/aoc2020/day1.clj
+++ src/aoc2020/day1.clj
@@ -1,4 +1,28 @@
 (ns aoc2020.day1
   (:gen-class))
 
-(def input (slurp "resources/day1.edn"))
+(defn pairs-that-sums-to-2020 [input]
+  (filter some?
+          (for [i input
+                j input]
+            (if (and
+                 (< i j)
+                 (= (+ i j) 2020))
+              [i j]))))
+
+(defn answer1 [input]
+  (apply * (first (pairs-that-sums-to-2020 input))))
+
+
+(defn triples-that-sums-to-2020 [input]
+  (filter some?
+          (for [i input
+                j input
+                k input]
+            (if (and
+                 (< i j k)
+                 (= (+ i j k) 2020))
+              [i j k]))))
+
+(defn answer2 [input]
+  (apply * (first (triples-that-sums-to-2020 input))))
--- test/aoc2020/day1_test.clj
+++ test/aoc2020/day1_test.clj
@@ -2,3 +2,15 @@
   (:require [clojure.test :refer :all]
             [aoc2020.day1 :refer :all]))
 
+(def input (read-string (slurp "resources/day1.edn")))
+
+(deftest check-answer1
+  (testing "Answer part 1 is correct"
+           (is (= (answer1 input) 1019571))))
+
+
+(deftest check-answer2
+  (testing "Answer part 2 is correct"
+           (is (= (answer2 input) 100655544))))
+
+(answer1 input)
