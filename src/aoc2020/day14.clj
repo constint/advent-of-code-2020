a11d8d6299003c316cfc10597c818c346ae1a32f
constint <cmanteau@talend.com>
2020-12-26 22:38:24 +0100

    Day 14 part 2

--- src/aoc2020/day14.clj
+++ src/aoc2020/day14.clj
@@ -6,7 +6,7 @@
     (long (Math/pow 2 idx))
     0))
 
-(defn bitstring->int [str]
+(defn bitseq->int [str]
   (reduce + (map-indexed bit->int (reverse str))))
 
 (defn or-mask
@@ -14,14 +14,14 @@
   [string]
   (->
    (clojure.string/replace string #"X" "0")
-   bitstring->int))
+   bitseq->int))
 
 (defn and-mask
   "And mask to turn bits to 0"
   [string]
   (->
    (clojure.string/replace string #"X" "1")
-   bitstring->int))
+   bitseq->int))
 
 (defn apply-mask [mask value]
   (-> value
@@ -62,6 +62,55 @@
   (->> input
        parse-input
        (reduce apply-changes
+               {} ; Initial memory state (nothing is zero)
+               )
+       (vals) ;; Keep only memory values
+       (reduce +) ;; Sum all values
+       ))
+
+;; PART 2
+(defn pad-to-36 [coll]
+  (take 36 (concat coll (repeat 0))))
+
+(defn x-mask [value mask]
+  (let [bit-chars-r   (reverse (Long/toBinaryString value))
+        mask-chars-r  (reverse mask)
+        new-val-r     (map (fn [mask-char bit-char] (if (= \X mask-char) \X bit-char))
+                           (pad-to-36 mask-chars-r) (pad-to-36 bit-chars-r))]
+    (apply str (reverse new-val-r))))
+
+(defn apply-mask-v2 [mask value]
+  (-> value
+      (bit-or (or-mask mask))
+      (x-mask mask)))
+
+(defn expand-address [[x & addr-rest]]
+  (if (empty? addr-rest)
+    (if (= \X x)
+      [[\0] [\1]]
+      [[x]])
+    (let [exansion-rest (expand-address addr-rest)]
+      (if (= \X x)
+        (concat (map #(cons \0 %) exansion-rest)
+                (map #(cons \1 %) exansion-rest))
+        (map #(cons x %) exansion-rest)))))
+
+;; Memory operations v2
+
+(defn apply-with-mask-v2 [mask memory {:keys [target value]}]
+  (let [mem-addresses  (map bitseq->int (expand-address (apply-mask-v2 mask target)))]
+    ;(prn "writing val" value "to addresses" mem-addresses)
+    (reduce (fn [mem addr] (assoc mem addr value))
+            memory
+            mem-addresses)))
+
+(defn apply-changes-v2 [memory {:keys [mask mem-assignments]}]
+  (reduce (partial apply-with-mask-v2 mask) memory mem-assignments))
+
+(defn input->answer2 [input]
+  (->> input
+       parse-input
+       (reduce apply-changes-v2
                {} ; Initial memory state (nothing is zero)
                )
        (vals) ;; Keep only memory values
--- test/aoc2020/day14_test.clj
+++ test/aoc2020/day14_test.clj
@@ -15,7 +15,38 @@
 (def input (slurp "resources/day14.txt"))
 
 (deftest check-answer1
-  (testing "Answer part 2 is correct"
+  (testing "Answer part 1 is correct"
            (is
             (= (input->answer1 input) 8471403462063))))
 
+(deftest masking-test-v2
+  (testing "Check with simple test"
+           (is
+            (= (apply-mask-v2 "000000000000000000000000000000X1001X" 42) "000000000000000000000000000000X1101X"))
+           (is
+            (= (apply-mask-v2 "00000000000000000000000000000000X0XX" 26) "00000000000000000000000000000001X0XX"))))
+
+(deftest address-expansion
+  (testing "Check with simple test"
+           (is
+            (= (map bitseq->int (expand-address (seq "X1101X")))
+               '(26 27 58 59)))
+           (is
+            (= (map bitseq->int (expand-address (seq "1X0XX")))
+               '(16 17 18 19 24 25 26 27)))))
+
+
+(deftest v2-test-input
+  (testing "Check simple v2"
+           (is
+             (=
+               (input->answer2 "mask = 000000000000000000000000000000X1001X
+mem[42] = 100
+mask = 00000000000000000000000000000000X0XX
+mem[26] = 1")
+               208))))
+
+(deftest check-answer2
+  (testing "Answer part 2 is correct"
+           (is
+            (= (input->answer2 input) 2667858637669))))
