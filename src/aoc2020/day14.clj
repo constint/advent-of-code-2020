6bebd2fcf9c84a6dd3cf1cf05d71d853456fc591
constint <cmanteau@talend.com>
2020-12-26 20:56:26 +0100

    Day 14 - Part 1

--- src/aoc2020/day14.clj
+++ src/aoc2020/day14.clj
@@ -27,3 +27,43 @@
   (-> value
       (bit-or (or-mask mask))
       (bit-and (and-mask mask))))
+
+;; Parsing
+
+(defn- parse-mem-assign [mem-str]
+  (let [[_ target value] (re-matches #"mem\[(\d+)\] = (\d+)" mem-str)]
+    {:target (Integer/parseInt target)
+     :value  (Integer/parseInt value)}))
+
+(defn- parse-group [[mask-str & mem-strs]]
+  {:mask            (second (re-matches #"mask = (.+)" mask-str))
+   :mem-assignments (map parse-mem-assign mem-strs)})
+
+(defn parse-input
+  "Parse input to format:
+  ({:mask \"111101X010011110100100110100101X0X0X\", :mem-assignments ({:target 37049, :value 1010356} {:target 5632, :value 28913} {:target 9384, :value 7522})} "
+  [input]
+  (let [mask-and-assignment-groups (->> input
+                                        .trim
+                                        (clojure.string/split-lines)
+                                        (partition-by #(.startsWith % "mask"))
+                                        (partition 2)
+                                        (map flatten))]
+    (map parse-group mask-and-assignment-groups)))
+
+;; Memory operations
+(defn apply-with-mask [mask memory {:keys [target value]}]
+  (assoc memory target (apply-mask mask value)))
+
+(defn apply-changes [memory {:keys [mask mem-assignments]}]
+  (reduce (partial apply-with-mask mask) memory mem-assignments))
+
+(defn input->answer1 [input]
+  (->> input
+       parse-input
+       (reduce apply-changes
+               {} ; Initial memory state (nothing is zero)
+               )
+       (vals) ;; Keep only memory values
+       (reduce +) ;; Sum all values
+       ))
--- test/aoc2020/day14_test.clj
+++ test/aoc2020/day14_test.clj
@@ -12,7 +12,10 @@
            (is
             (= (apply-mask "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X" 0) 64))))
 
+(def input (slurp "resources/day14.txt"))
 
+(deftest check-answer1
+  (testing "Answer part 2 is correct"
+           (is
+            (= (input->answer1 input) 8471403462063))))
 
-
-(run-tests *ns*)
